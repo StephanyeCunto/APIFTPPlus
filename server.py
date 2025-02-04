75dc14641adc7eaf98e874a70466cc87d4ea79c7
StephanyeCunto <stephanyecristine6@gmail.com>
2025-02-04 07:47:18 -0300

    Melhorias na classes server.py

--- server.py
+++ server.py
@@ -1,91 +1,103 @@
 import socket
 import os
-import shutil
 
-def servidor():
-    host = 'localhost'  
-    porta = 12345  
-
+def criar_diretorio():
     if not os.path.exists('./arquivos'):
         os.makedirs('./arquivos')
 
+def listar_arquivos():
+    return str(os.listdir('./arquivos')).encode()
+
+def excluir_arquivo(comando):
+    try:
+        nome_do_arquivo = comando.split(' ', 1)[1]
+        caminho_arquivo = os.path.join('./arquivos', nome_do_arquivo)
+        
+        if os.path.exists(caminho_arquivo):
+            os.remove(caminho_arquivo)
+            return f'Arquivo excluído: {nome_do_arquivo}'.encode()
+        else:
+            return f'Arquivo não encontrado: {nome_do_arquivo}'.encode()
+    except IndexError:
+        return b'Erro: Nome do arquivo nao especificado'
+
+def receber_arquivo(conn, comando):
+    try:
+        nome_do_arquivo = comando.split(' ', 1)[1]
+        caminho_destino = os.path.join('./arquivos', nome_do_arquivo)
+        
+        conn.send(b'OK')
+        
+        with open(caminho_destino, 'wb') as arquivo:
+            while True:
+                dados = conn.recv(1024)
+                if not dados or dados == b"EOF":
+                    break
+                arquivo.write(dados)
+        
+        return f'Arquivo {nome_do_arquivo} recebido com sucesso.'.encode()
+    except IndexError:
+        return b'Erro: Nome do arquivo nao especificado'
+
+def enviar_arquivo(conn, comando):
+    try:
+        nome_do_arquivo = comando.split(' ', 1)[1]
+        caminho_arquivo = os.path.join('./arquivos', nome_do_arquivo)
+        
+        if not os.path.exists(caminho_arquivo):
+            return 'Erro: Arquivo não encontrado.'.encode()
+        
+        conn.send(b'OK')
+        resposta = conn.recv(1024).decode()
+        
+        if resposta != "Pronto para receber":
+            return b'Erro na transferencia'
+        
+        with open(caminho_arquivo, 'rb') as arquivo:
+            while chunk := arquivo.read(1024):
+                conn.send(chunk)
+        
+        conn.send(b"EOF")
+        return None  # Indica que o envio foi concluído
+    except IndexError:
+        return b'Erro: Nome do arquivo nao especificado'
+
+def processar_comando(conn, comando):
+    match comando.split()[0]:
+        case "listar":
+            return listar_arquivos()
+        case "excluir":
+            return excluir_arquivo(comando)
+        case "enviar":
+            return receber_arquivo(conn, comando)
+        case "download":
+            return enviar_arquivo(conn, comando)
+        case _:
+            return 'Comando não reconhecido'.encode('utf-8')
+
+def servidor():
+    criar_diretorio()
+    host, porta = 'localhost', 12345
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, porta))
     s.listen(5)
-
+    
     print(f'Servidor escutando na porta {porta}')
-
+    
     while True:
-        conn, addr = s.accept()  
+        conn, addr = s.accept()
         print(f'Conexão estabelecida com {addr}')
-
+        
         try:
-            comando = conn.recv(1024).decode() 
-
-            if comando == "listar":
-                arquivos = os.listdir('./arquivos')  
-                conn.send(str(arquivos).encode()) 
-
-            elif comando.startswith("excluir"):
-                nome_do_arquivo = comando.split(' ')[1]
-                caminho_arquivo = os.path.join('./arquivos', nome_do_arquivo)
-
-                if os.path.exists(caminho_arquivo):
-                    os.remove(caminho_arquivo)
-                    resposta = f'Arquivo excluído: {nome_do_arquivo}'
-                else:
-                    resposta = f'Arquivo não encontrado: {nome_do_arquivo}'
-
-                conn.send(resposta.encode())
-
-            elif comando.startswith("enviar"):
-                nome_do_arquivo = comando.split(' ', 1)[1]
-                caminho_destino = os.path.join('./arquivos', nome_do_arquivo)
-
-                conn.send("OK".encode())  
-
-                with open(caminho_destino, 'wb') as arquivo:
-                    while True:
-                        dados = conn.recv(1024)
-                        if not dados or dados == b"EOF":
-                            break
-                        arquivo.write(dados)
-
-                conn.send(f'Arquivo {nome_do_arquivo} recebido com sucesso.'.encode())
-
-            elif comando.startswith("download"):
-                nome_do_arquivo = comando.split(' ', 1)[1]
-                caminho_arquivo = os.path.join('./arquivos', nome_do_arquivo)
-
-                if not os.path.exists(caminho_arquivo):
-                    conn.send("Erro: Arquivo não encontrado.".encode())
-                    conn.close()
-                    continue
-
-                conn.send("OK".encode())
-                resposta = conn.recv(1024).decode()  
-
-                if resposta != "Pronto para receber":
-                    conn.close()
-                    continue
-
-                with open(caminho_arquivo, 'rb') as arquivo:
-                    while True:
-                        dados = arquivo.read(1024)
-                        if not dados:
-                            break
-                        conn.send(dados)
-
-                conn.send(b"EOF") 
-
-            else:
-                conn.send("Comando não reconhecido".encode())
-
+            comando = conn.recv(1024).decode()
+            resposta = processar_comando(conn, comando)
+            if resposta:
+                conn.send(resposta)
         except Exception as e:
             print(f"Erro durante a conexão: {e}")
-
         finally:
-            conn.close() 
+            conn.close()
 
 if __name__ == "__main__":
     servidor()
